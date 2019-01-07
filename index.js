4f4ad5662bcdb55462302487c1c9ab88e0a6270f
Son_dang <son.dang@doda100.com>
2019-01-07 20:56:28 +0700

    fix port

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
+const port = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
-  console.log("JSON Server is running at http://localhost:8080");
+server.listen(port, () => {
+  console.log("JSON Server is running at http://localhost:" + port);
 });
