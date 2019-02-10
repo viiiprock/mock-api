3e6ede34bad8e2775b0f9575a4f360055795290f
Son_dang <son.dang@doda100.com>
2019-02-10 09:12:02 +0700

    test

---
+++ fake/polls.js
@@ -0,0 +1,389 @@
+module.exports = [
+  {
+    id: 1,
+    question: "Lonni",
+    options: null,
+    threadId: 29
+  },
+  {
+    id: 2,
+    question: "Andie",
+    options: [
+      {
+        id: 1,
+        name: "Taclobo",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 9
+  },
+  {
+    id: 3,
+    question: "Alair",
+    options: [
+      {
+        id: 1,
+        name: "Huaraz",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Nadvoitsy",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Dalsjöfors",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Banyulegi",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 5,
+        name: "Basail",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 5
+  },
+  {
+    id: 4,
+    question: "Elwyn",
+    options: [
+      {
+        id: 1,
+        name: "Shanguang",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 19
+  },
+  {
+    id: 5,
+    question: "Legra",
+    options: null,
+    threadId: 6
+  },
+  {
+    id: 6,
+    question: "Mercie",
+    options: [
+      {
+        id: 1,
+        name: "Xin’an",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Ushuaia",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Krajan",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Sosándra",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 24
+  },
+  {
+    id: 7,
+    question: "Nancee",
+    options: [
+      {
+        id: 1,
+        name: "Minuwangoda",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Pato Branco",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Ushirombo",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Cimo de Vila",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 5,
+        name: "Tuzha",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 10
+  },
+  {
+    id: 8,
+    question: "Laurice",
+    options: [
+      {
+        id: 1,
+        name: "Novo",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Guangchen",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Axioúpoli",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Sovetakan",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 19
+  },
+  {
+    id: 9,
+    question: "Freemon",
+    options: [
+      {
+        id: 1,
+        name: "Străşeni",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 19
+  },
+  {
+    id: 10,
+    question: "Hershel",
+    options: [
+      {
+        id: 1,
+        name: "Miaoshou",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Changsha",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 14
+  },
+  {
+    id: 11,
+    question: "Liliane",
+    options: [
+      {
+        id: 1,
+        name: "Bánov",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Īlām",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Kota Bharu",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Sambogunung",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 27
+  },
+  {
+    id: 12,
+    question: "Gaby",
+    options: [
+      {
+        id: 1,
+        name: "Pingding",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Khao Yoi",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Višnjićevo",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Ribeira Grande",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 21
+  },
+  {
+    id: 13,
+    question: "Shirl",
+    options: [
+      {
+        id: 1,
+        name: "Alajuela",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Acherítou",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Sruki",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 5
+  },
+  {
+    id: 14,
+    question: "Lion",
+    options: [
+      {
+        id: 1,
+        name: "Hoxtolgay",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Xiachengzi",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Sarakhs",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 18
+  },
+  {
+    id: 15,
+    question: "Shawna",
+    options: [
+      {
+        id: 1,
+        name: "La Soledad",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 16
+  },
+  {
+    id: 16,
+    question: "Shoshanna",
+    options: [
+      {
+        id: 1,
+        name: "Zhonghekou",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Nagano-shi",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Bakung Utara",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Baizhifang",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 23
+  },
+  {
+    id: 17,
+    question: "Noellyn",
+    options: [
+      {
+        id: 1,
+        name: "Älvdalen",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Tonggoh",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 3,
+        name: "Nizhnepavlovka",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 4,
+        name: "Quebracho",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 5,
+        name: "Sarov",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 20
+  },
+  {
+    id: 18,
+    question: "Peder",
+    options: [
+      {
+        id: 1,
+        name: "Karangasem",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        id: 2,
+        name: "Radviliskis",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 18
+  },
+  {
+    id: 19,
+    question: "Marlowe",
+    options: null,
+    threadId: 11
+  },
+  {
+    id: 20,
+    question: "Valida",
+    options: [
+      {
+        id: 1,
+        name: "El Retorno",
+        voteUserIDs: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    threadId: 27
+  }
+];
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const jsonGraphqlExpress = require("json-graphql-server");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const deepClone = require("./deepClone");
 const data = require("./data");
+const polls = require("./fake/polls");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const mockDatas = { ...data, polls: deepClone(polls) };
+console.log(mockDatas.polls);
 
 app.use("/graphql", cors(), bodyParser.json(), jsonGraphqlExpress.default(data));
 app.listen(PORT, () =>
