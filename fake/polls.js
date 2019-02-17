65098aeaa1fdf98181f643e3d88a41f2d5df00b6
Son_dang <son.dang@doda100.com>
2019-02-18 06:56:35 +0700

    update poll data

--- fake/polls.js
+++ fake/polls.js
@@ -1,750 +1,383 @@
-module.exports = [{
-  "id": 1,
-  "question": "What is Mara?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Vanimo",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Aoshi",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Sirnaresmi",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 28,
-  "closed": true
-}, {
-  "id": 2,
-  "question": "What is Hoffman's sloth?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Lena",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Isiro",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 9,
-  "closed": false
-}, {
-  "id": 3,
-  "question": "What is Asian red fox?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Ollantaytambo",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Paicol",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 30,
-  "closed": true
-}, {
-  "id": 4,
-  "question": "What is Boar, wild?",
-  "options": null,
-  "threadID": null,
-  "closed": false
-}, {
-  "id": 5,
-  "question": "What is Southern lapwing?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Tsuyama",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Pégeia",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 8,
-  "closed": true
-}, {
-  "id": 6,
-  "question": "What is Fork-tailed drongo?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Nova Venécia",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 15,
-  "closed": true
-}, {
-  "id": 7,
-  "question": "What is Sage grouse?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Svorónos",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Boston",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Ndélé",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 21,
-  "closed": false
-}, {
-  "id": 8,
-  "question": "What is African pied wagtail?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Yanqi",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Port Barton",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Paris 16",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 22,
-  "closed": false
-}, {
-  "id": 9,
-  "question": "What is Gecko, ring-tailed?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Citeureup",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Kępice",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Bajomulyo",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Bethlehem",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 7,
-  "closed": false
-}, {
-  "id": 10,
-  "question": "What is Peacock, blue?",
-  "options": null,
-  "threadID": null,
-  "closed": false
-}, {
-  "id": 11,
-  "question": "What is Legaan, water?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Kraczkowa",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Krajan",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Drohobych",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Kaseda-shirakame",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 15,
-  "closed": false
-}, {
-  "id": 12,
-  "question": "What is Arctic lemming?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Porsgrunn",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Balatero",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Rodniki",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Barbacoas",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 5,
-      "name": "Shancheng",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 24,
-  "closed": true
-}, {
-  "id": 13,
-  "question": "What is Duck, mountain?",
-  "options": [
-    {
-      "id": 1,
-      "name": "El Carmen",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Tangchi",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Barurao",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 6,
-  "closed": false
-}, {
-  "id": 14,
-  "question": "What is Leopard, indian?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Nishiwaki",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 28,
-  "closed": false
-}, {
-  "id": 15,
-  "question": "What is Crane, stanley?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Hukou",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Łagów",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Lysice",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Wates",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 17,
-  "closed": false
-}, {
-  "id": 16,
-  "question": "What is Snake, tiger?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Tempe",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Nagpandayan",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Bakungan",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 13,
-  "closed": false
-}, {
-  "id": 17,
-  "question": "What is Pacific gull?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Konosha",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Guanzhou",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Borodinskiy",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Leipsoí",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 5,
-      "name": "Tha Luang",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 7,
-  "closed": false
-}, {
-  "id": 18,
-  "question": "What is Gazelle, thomson's?",
-  "options": null,
-  "threadID": null,
-  "closed": true
-}, {
-  "id": 19,
-  "question": "What is Buffalo, asian water?",
-  "options": [
-    {
-      "id": 1,
-      "name": "Kalembutillu",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Tiguha",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Orlando",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 4,
-      "name": "Brades",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    },
-    {
-      "id": 5,
-      "name": "Bourg-en-Bresse",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 15,
-  "closed": true
-}, {
-  "id": 20,
-  "question": "What is White-nosed coatimundi?",
-  "options": [
-    {
-      "id": 1,
-      "name": "København",
-      "voteUserIDs": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-      ]
-    }
-  ],
-  "threadID": 9,
-  "closed": false
-}]
+module.exports = [
+  {
+    id: 1,
+    question: "What is Wallaby, tammar?",
+    options: [
+      {
+        id: 1,
+        name: "Shiziling",
+        voteUserIDs: [1, 4, 8, 9]
+      },
+      {
+        id: 2,
+        name: "Tolkmicko",
+        voteUserIDs: [7, 2]
+      },
+      {
+        id: 3,
+        name: "Intendente Alvear",
+        voteUserIDs: [8, 1, 6]
+      },
+      {
+        id: 4,
+        name: "Buenaventura",
+        voteUserIDs: [1, 7, 3, 9, 10, 11]
+      }
+    ],
+    threadID: 10,
+    closed: true
+  },
+  {
+    id: 2,
+    question: "What is Burmese brown mountain tortoise?",
+    options: [
+      {
+        id: 1,
+        name: "Tamansari",
+        voteUserIDs: [6, 12, 8, 2]
+      },
+      {
+        id: 2,
+        name: "Kannabechō-yahiro",
+        voteUserIDs: [10, 16, 8, 18, 19, 12]
+      },
+      {
+        id: 3,
+        name: "Wangzuo",
+        voteUserIDs: [10, 16, 8, 14, 11, 13, 14]
+      }
+    ],
+    threadID: 16,
+    closed: true
+  },
+  {
+    id: 3,
+    question: "What is Hyena, striped?",
+    options: null,
+    threadID: null,
+    closed: true
+  },
+  {
+    id: 4,
+    question: "What is Langur, hanuman?",
+    options: [
+      {
+        id: 1,
+        name: "Nhà Bàng",
+        voteUserIDs: [16, 20, 28, 24]
+      }
+    ],
+    threadID: 19,
+    closed: true
+  },
+  {
+    id: 5,
+    question: "What is American alligator?",
+    options: [
+      {
+        id: 1,
+        name: "Dayou",
+        voteUserIDs: [35, 10, 45, 20]
+      },
+      {
+        id: 2,
+        name: "Xihe",
+        voteUserIDs: [35, 25, 25, 15]
+      },
+      {
+        id: 3,
+        name: "Vichadero",
+        voteUserIDs: [15, 35, 10, 15]
+      },
+      {
+        id: 4,
+        name: "Arlington",
+        voteUserIDs: [30, 15, 20, 5]
+      },
+      {
+        id: 5,
+        name: "Jes María",
+        voteUserIDs: [10, 15, 25, 45]
+      }
+    ],
+    threadID: 13,
+    closed: false
+  },
+  {
+    id: 6,
+    question: "What is Kudu, greater?",
+    options: [
+      {
+        id: 1,
+        name: "Al Qubbah",
+        voteUserIDs: [48, 24, 54, 30]
+      }
+    ],
+    threadID: 6,
+    closed: false
+  },
+  {
+    id: 7,
+    question: "What is Possum, western pygmy?",
+    options: [
+      {
+        id: 1,
+        name: "Lipník nad Bečvou",
+        voteUserIDs: [14, 63, 7, 56]
+      }
+    ],
+    threadID: 26,
+    closed: false
+  },
+  {
+    id: 8,
+    question: "What is Leadbeateri's ground hornbill?",
+    options: [
+      {
+        id: 1,
+        name: "Blawi",
+        voteUserIDs: [72, 72, 40, 32]
+      }
+    ],
+    threadID: 16,
+    closed: false
+  },
+  {
+    id: 9,
+    question: "What is Western patch-nosed snake?",
+    options: [
+      {
+        id: 1,
+        name: "Planaltina",
+        voteUserIDs: [9, 81, 72, 81]
+      },
+      {
+        id: 2,
+        name: "Tolbazy",
+        voteUserIDs: [63, 45, 81, 9]
+      },
+      {
+        id: 3,
+        name: "Cuiabá",
+        voteUserIDs: [36, 9, 45, 27]
+      }
+    ],
+    threadID: 17,
+    closed: true
+  },
+  {
+    id: 10,
+    question: "What is Bustard, stanley?",
+    options: [
+      {
+        id: 1,
+        name: "Lüderitz",
+        voteUserIDs: [60, 10, 60, 70]
+      },
+      {
+        id: 2,
+        name: "Eisen",
+        voteUserIDs: [70, 90, 40, 70]
+      }
+    ],
+    threadID: 12,
+    closed: true
+  },
+  {
+    id: 11,
+    question: "What is Egret, cattle?",
+    options: [
+      {
+        id: 1,
+        name: "São Borja",
+        voteUserIDs: [55, 33, 11, 22]
+      }
+    ],
+    threadID: 29,
+    closed: true
+  },
+  {
+    id: 12,
+    question: "What is Baboon, olive?",
+    options: [
+      {
+        id: 1,
+        name: "Darpas",
+        voteUserIDs: [72, 72, 72, 36]
+      },
+      {
+        id: 2,
+        name: "Diofior",
+        voteUserIDs: [36, 24, 108, 12]
+      },
+      {
+        id: 3,
+        name: "Guling",
+        voteUserIDs: [96, 36, 36, 36]
+      },
+      {
+        id: 4,
+        name: "Düsseldorf",
+        voteUserIDs: [72, 108, 96, 72]
+      }
+    ],
+    threadID: 5,
+    closed: true
+  },
+  {
+    id: 13,
+    question: "What is Shelduck, european?",
+    options: [
+      {
+        id: 1,
+        name: "Gongchenqiao",
+        voteUserIDs: [117, 117, 52, 78]
+      },
+      {
+        id: 2,
+        name: "Moravský Beroun",
+        voteUserIDs: [13, 39, 78, 13]
+      },
+      {
+        id: 3,
+        name: "Shucheng Chengguanzhen",
+        voteUserIDs: [39, 39, 26, 117]
+      },
+      {
+        id: 4,
+        name: "Hostouň",
+        voteUserIDs: [26, 65, 78, 39]
+      },
+      {
+        id: 5,
+        name: "Drochia",
+        voteUserIDs: [26, 91, 39, 65]
+      }
+    ],
+    threadID: 27,
+    closed: true
+  },
+  {
+    id: 14,
+    question: "What is Monitor lizard (unidentified)?",
+    options: null,
+    threadID: null,
+    closed: true
+  },
+  {
+    id: 15,
+    question: "What is Bird, bare-faced go away?",
+    options: null,
+    threadID: null,
+    closed: false
+  },
+  {
+    id: 16,
+    question: "What is Eastern fox squirrel?",
+    options: null,
+    threadID: null,
+    closed: false
+  },
+  {
+    id: 17,
+    question: "What is Eagle owl (unidentified)?",
+    options: [
+      {
+        id: 1,
+        name: "Meybod",
+        voteUserIDs: [153, 85, 51, 68]
+      },
+      {
+        id: 2,
+        name: "Tianya",
+        voteUserIDs: [34, 68, 51, 153]
+      }
+    ],
+    threadID: 15,
+    closed: false
+  },
+  {
+    id: 18,
+    question: "What is Baboon, savanna?",
+    options: [
+      {
+        id: 1,
+        name: "Ketanggungan",
+        voteUserIDs: [144, 54, 36, 108]
+      },
+      {
+        id: 2,
+        name: "Dresden",
+        voteUserIDs: [18, 90, 108, 162]
+      },
+      {
+        id: 3,
+        name: "Sokolovyy",
+        voteUserIDs: [54, 126, 162, 72]
+      },
+      {
+        id: 4,
+        name: "Petroúpolis",
+        voteUserIDs: [108, 162, 126, 72]
+      }
+    ],
+    threadID: 25,
+    closed: true
+  },
+  {
+    id: 19,
+    question: "What is Egyptian goose?",
+    options: [
+      {
+        id: 1,
+        name: "Bylym",
+        voteUserIDs: [114, 95, 95, 133]
+      },
+      {
+        id: 2,
+        name: "Wenqiao",
+        voteUserIDs: [57, 114, 95, 171]
+      },
+      {
+        id: 3,
+        name: "Wiślica",
+        voteUserIDs: [95, 19, 38, 19]
+      },
+      {
+        id: 4,
+        name: "Nantes",
+        voteUserIDs: [114, 76, 57, 57]
+      },
+      {
+        id: 5,
+        name: "Bodega",
+        voteUserIDs: [152, 57, 114, 152]
+      }
+    ],
+    threadID: 13,
+    closed: true
+  },
+  {
+    id: 20,
+    question: "What is Square-lipped rhinoceros?",
+    options: [
+      {
+        id: 1,
+        name: "Conception Bay South",
+        voteUserIDs: [80, 120, 100, 40]
+      },
+      {
+        id: 2,
+        name: "Rio Negro",
+        voteUserIDs: [20, 60, 60, 20]
+      },
+      {
+        id: 3,
+        name: "Farrokh Shahr",
+        voteUserIDs: [20, 120, 100, 180]
+      }
+    ],
+    threadID: 7,
+    closed: false
+  }
+];
